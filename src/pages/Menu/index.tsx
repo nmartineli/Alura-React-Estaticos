5f34bc7ce1d6b257da5be228076589d5a0bdeaff
NataliaMartineli <nataliamartineli@gmail.com>
2022-04-11 18:43:22 -0300

    Header and CSS Variables

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -8,6 +8,9 @@ export default function Menu() {
 				<Logo />
 				aluroni
 			</nav>
+			<header className={styles.header}>
+				<div className={styles.header__text}>A casa do código e da massa</div>
+			</header>
 		</main>
 	);
 }
