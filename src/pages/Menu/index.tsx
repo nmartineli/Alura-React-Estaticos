a05ba65d7d6b3ead8d9462273d80f3892e4d8ab4
NataliaMartineli <nataliamartineli@gmail.com>
2022-04-11 19:07:55 -0300

    Search input + styles

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,7 +1,11 @@
 import styles from './Menu.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import Search from './Search';
+import { useState } from 'react';
 
 export default function Menu() {
+	const [search, setSearch] = useState('');
+
 	return (
 		<main>
 			<nav className={styles.menu}>
@@ -11,6 +15,10 @@ export default function Menu() {
 			<header className={styles.header}>
 				<div className={styles.header__text}>A casa do código e da massa</div>
 			</header>
+			<section className={styles.cardapio}>
+				<h3 className={styles.cardapio__titulo}>Cardápio</h3>
+				<Search search={search} setSearch={setSearch} />
+			</section>
 		</main>
 	);
 }
