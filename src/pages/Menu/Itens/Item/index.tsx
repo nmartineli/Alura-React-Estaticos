6dbf2f4ce6c4f73bea2179f136bade59505d2faa
NataliaMartineli <nataliamartineli@gmail.com>
2022-04-12 20:29:22 -0300

    Added item infos from API

--- src/pages/Menu/Itens/Item/index.tsx
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -1,21 +1,31 @@
 import styles from './Item.module.scss';
+import menu from '../itens.json';
+import classNames from 'classnames';
 
-export default function Item() {
+type Props = typeof menu[0];
+
+export default function Item(props: Props) {
+	const { title, description, category, size, serving, price, photo } = props;
+
 	return (
 		<div className={styles.item}>
 			<div className={styles.item__imagem}>
-				<img src="" alt="" />
+				<img src={photo} alt={title} />
 			</div>
 			<div className={styles.item__descricao}>
 				<div className={styles.item__titulo}>
-					<h2>Macarrão</h2>
-					<p>Descrição do macarrão</p>
+					<h2>{title}</h2>
+					<p>{description}</p>
 				</div>
 				<div className={styles.item__tags}>
-					<div className={styles.item__tipo}>Massa</div>
-					<div className={styles.item__porcao}>400g</div>
-					<div className={styles.item__qtdpessoas}>Serve 2 pessoas</div>
-					<div className={styles.item__valor}>R$ 50</div>
+					<div className={classNames(styles.item__tipo, styles[`item__tipo__${category.label.toLowerCase()}`])}>
+						{category.label}
+					</div>
+					<div className={styles.item__porcao}>{size}g</div>
+					<div className={styles.item__qtdpessoas}>
+						Serve {serving} pessoa{serving === 1 ? '' : 's'}
+					</div>
+					<div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
 				</div>
 			</div>
 		</div>
